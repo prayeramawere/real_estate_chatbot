4df6fd42878b8583351b6a3a59c5f9795f4b899b
Prayer Mawere <prayermawerea@gmail.com>
2026-04-21 11:29:41 +0200

    cors fix

--- packages/server/app.js
+++ packages/server/app.js
@@ -6,6 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://real-estate-chatbot-1-n700.onrender.com",
+  }),
+);
 
 app.use("/api/chat", chat);
 
